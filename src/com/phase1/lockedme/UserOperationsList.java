fb16a002561024e50f79bc2a0136e310f3ce1577
Ubuntu <ubuntu@ip-172-31-64-6.ec2.internal>
2020-08-15 04:07:25 +0000

    added fileOperations class

--- src/com/phase1/lockedme/UserOperationsList.java
+++ src/com/phase1/lockedme/UserOperationsList.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class UserOperationsList {
 
 	Operations op = new Operations();
-	
+
 	public void operationList() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("");
 		System.out.println("1 Fetch the User details");
 		System.out.println("2 Fetch the files in a folder");
 		System.out.println("3 Perform File operations");
-		System.out.println("6 Back");
+		System.out.println("4 Back");
 		System.out.println("***********************************");
- 
+
 		try {
 			int keyboardInput = input.nextInt();
 			switch (keyboardInput) {
@@ -25,20 +25,15 @@ public class UserOperationsList {
 				op.fetchFiles();
 				break;
 			case 3:
-				System.out.println("3 Add a file");
-				System.out.println("4 Delete a file");
-				System.out.println("5 Search for a file");
-				op.addFile();
+
+				FileOperations list = new FileOperations();
+				list.fileOperationList();
 				break;
+
 			case 4:
-				op.deleteFile();
-				break;
-			case 5:
-				op.searchFile();
-				break;
-			case 6:
 				Welcome user = new Welcome();
 				user.welcomeUser();
+			
 				break;
 			default:
 				System.out.println("Invalid input, Enter valid number");
@@ -50,6 +45,5 @@ public class UserOperationsList {
 			operationList();
 		}
 
-		
 	}
 }
